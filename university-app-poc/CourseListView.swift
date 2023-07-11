8aacceaa8abd2770334d7c6697a2b6ac1449c31c
Taha Chaudhry <taha.chaudhry@icloud.com>
2023-07-11 21:43:30 +0300

    first commit

---
+++ university-app-poc/CourseListView.swift
@@ -0,0 +1,20 @@
+//
+//  CourseListView.swift
+//  university-app-poc
+//
+//  Created by Taha Chaudhry on 11/07/2023.
+//
+
+import SwiftUI
+
+struct CourseListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CourseListView_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseListView()
+    }
+}
---
+++ university-app-poc/UniversityListView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchUniversities.swift
+//  university-app-poc
+//
+//  Created by Taha Chaudhry on 11/07/2023.
+//
+
+import SwiftUI
+
+struct SearchUniversities: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchUniversities_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchUniversities()
+    }
+}
